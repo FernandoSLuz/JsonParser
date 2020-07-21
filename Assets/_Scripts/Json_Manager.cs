f00d6da9a0a001bf909f1e20e07cff2aa3134f9f
FernandoSLuz <fernandosilvadaluz@gmail.com>
2020-07-21 20:27:46 -0300

    Specs ok
    -Use Unity 2018.1 or later version
    -Use the sample JSON to test, but do not count on it. Your code will be tested with other JSON content that may differ in the amount of columns, rows or data.
    -There is no need to support scrolling vertically or horizontally.
    -Write your processing code in C# scripts
    -Do not spend time making the UI pretty. Just read the data and present it on screen.
    -The process should run as a Standalone Desktop build (i.e, make sure to test creating a build and running it)
    -Please deliver the full Unity project source.

---
+++ Assets/_Scripts/Json_Manager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson; //Implemented for better manipulation of json data
+using System.IO;
+using UnityEngine.Networking;
+using TMPro;
+public class Json_Manager : MonoBehaviour
+{
+	//The prefabs to create the sheet
+	public GameObject rowPrefab;
+	public GameObject colPrefab;
+	//Panel where the objects will be instantiated
+	public Transform parent;
+	private IEnumerator Start()
+	{
+		//Populate json file on streamingAssets path
+		string filePath = Path.Combine(Application.streamingAssetsPath, "JsonChallenge.json");
+		UnityWebRequest www = UnityWebRequest.Get(filePath);
+		yield return www.SendWebRequest();
+		string json = www.downloadHandler.text;
+		//convert json string into object
+		JsonData data = JsonMapper.ToObject(json);
+		Populate(data);
+	}
+	
+	public void Populate(JsonData data){
+		//create list with all headers, and then populate with the ones in json
+		List<string> ColumnHeaders = new List<string>();
+		foreach (JsonData item in data["ColumnHeaders"]){
+			ColumnHeaders.Add((string)item);
+		}
+		//Create first row with title 
+		InstantiateRowAndColumns(new List<string>() { (string)data["Title"] });
+		//Create second row with guide columns 
+		InstantiateRowAndColumns(ColumnHeaders);
+		//Loop through Data array and then populate each row with all data
+		foreach (JsonData item in data["Data"])
+		{
+			InstantiateRowAndColumns(ColumnHeaders, item);
+		}
+	}
+	public void InstantiateRowAndColumns(List<string> tempColumnHeaders, JsonData data = null)
+	{
+		GameObject row = Instantiate(rowPrefab, parent, false);
+		foreach(var item in tempColumnHeaders)
+		{
+			GameObject col = Instantiate(colPrefab, row.transform, false);
+			TextMeshProUGUI text = col.GetComponentInChildren<TextMeshProUGUI>();
+			//if that is null, then we know is title or guide collumns
+			if(data == null){
+				text.fontStyle = FontStyles.Bold;
+				text.text = item;
+			}else{
+				//Verify if the object contains the specific key
+				if (JsonDataContainsKey(data, item))
+				{
+					text.text = (string)data[item];
+				}
+				else
+				{
+					//if not, just show that on the sheet
+					text.text = "--";
+				}
+			}
+		}
+	}
+	// Json utility method to verify if key exist in specified object
+	static public bool JsonDataContainsKey(JsonData data, string key)
+	{
+		bool result = false;
+		if (data == null)
+			return result;
+		if (!data.IsObject)
+		{
+			return result;
+		}
+		IDictionary tdictionary = data as IDictionary;
+		if (tdictionary == null)
+			return result;
+		if (tdictionary.Contains(key))
+		{
+			result = true;
+		}
+		return result;
+	}
+}
